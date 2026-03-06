5983348306e12d551206ef94e9c4a88fbd9530a9
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-05 21:50:46 -0300

    Add files via upload

--- to_do-list.py
+++ to_do-list.py
@@ -1,21 +1,43 @@
 tasks = []
 
 def show_menu():
-    print("To do list \n1- Add task \n2-View tasks \n3- Mark task as done \n4- Delete task \n5- Close app")
+    print("To do list \n1- Add task \n2- View tasks \n3- Mark task as done \n4- Delete task \n5- Close app")
+    options = list(range(1,6))
+
+    while True:
+        while True:
+            try:
+                action = int(input("\nAction: "))
+                if action not in options:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid value")
+
+        if action == 1:
+            add_task()
+        elif action == 2:
+            view_task()
+        elif action == 3:
+            task_done()
+        elif action == 4:
+            delete_task()
+        elif action == 5:
+            print("Closing program...")
+            break
+        show_menu()
+        break
 
 def add_task():
-    print("Adding task...")
+    print("Adding task...\n")
 
 def view_task():
-    print("Viewing task...")
+    print("Viewing task...\n")
 
 def task_done():
-    print("Setting task as done...")
+    print("Setting task as done...\n")
 
 def delete_task():
-    print("Deleting task...")
-
-def close_app():
-    print("Closing program...")
+    print("Deleting task...\n")
 
 show_menu()
