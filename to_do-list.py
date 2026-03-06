ed1deb6e4af868e00982895b589ba2227539aa70
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-05 21:38:18 -0300

    Add files via upload

--- to_do-list.py
+++ to_do-list.py
@@ -1,9 +1,21 @@
 tasks = []
 
 def show_menu():
-    print("To do list \n1- Add task \n2-View task \n3- Mark task as done \n4- Delete task \n5- Close app")
-
-show_menu()
+    print("To do list \n1- Add task \n2-View tasks \n3- Mark task as done \n4- Delete task \n5- Close app")
 
 def add_task():
-    pass
+    print("Adding task...")
+
+def view_task():
+    print("Viewing task...")
+
+def task_done():
+    print("Setting task as done...")
+
+def delete_task():
+    print("Deleting task...")
+
+def close_app():
+    print("Closing program...")
+
+show_menu()
