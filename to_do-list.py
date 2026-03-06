8935c10a98ca96a0ee63ae4f0fb86945f5dd09a5
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-05 21:33:56 -0300

    Add files via upload

---
+++ to_do-list.py
@@ -0,0 +1,9 @@
+tasks = []
+
+def show_menu():
+    print("To do list \n1- Add task \n2-View task \n3- Mark task as done \n4- Delete task \n5- Close app")
+
+show_menu()
+
+def add_task():
+    pass
